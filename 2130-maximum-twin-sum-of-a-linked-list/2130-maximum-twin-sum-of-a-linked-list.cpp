0abaa1affb834e8964f28db1f924a202d67ec0d8
kpsingh-20 <75653821+kpsingh-20@users.noreply.github.com>
2022-12-05 09:42:33 +0530

    Time: 340 ms (77.62%), Space: 116.7 MB (75.74%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.cpp
@@ -11,26 +11,63 @@
 
 #define 	Node				ListNode
 
+
+// This solution uses O(N) space but below solution uses O(1) space as we have reverese the other half of the linkedlist so that we can traverse simultaneously.
 class Solution {
 public:
+// int pairSum(Node* head) 
+// {
+// 	std::vector<int> v;
+// 	int n = 0;
+
+// 	Node *temp = head;
+// 	while(temp){
+// 		n++;
+// 		v.push_back(temp->val);
+// 		temp = temp->next;
+// 	}
+
+// 	int l = 0, r = n-1;
+// 	int ans = INT_MIN;
+// 	while(l<r){
+// 		ans = max(ans, v[l++] + v[r--]);
+// 	}
+
+// 	return ans;
+// }
+    
+    
 int pairSum(Node* head) 
 {
-	std::vector<int> v;
-	int n = 0;
+	Node *slow = head;
+	Node * fast = head;
 
-	Node *temp = head;
-	while(temp){
-		n++;
-		v.push_back(temp->val);
-		temp = temp->next;
+	while(fast and fast->next){
+		slow = slow->next;
+		fast = fast->next->next;
 	}
 
-	int l = 0, r = n-1;
+	Node *pre = NULL;
+	Node * temp;
+	while(slow){
+		temp = slow->next;
+		slow->next = pre;
+		pre = slow;
+		slow = temp;
+	}
+
+
+	// display(head);br;
+	// display(pre);br;
+
 	int ans = INT_MIN;
-	while(l<r){
-		ans = max(ans, v[l++] + v[r--]);
+	fast = head;
+	while(pre){
+		ans = max(ans, pre->val + fast->val);
+		pre = pre->next;
+		fast = fast->next;
 	}
-
 	return ans;
 }
+
 };
