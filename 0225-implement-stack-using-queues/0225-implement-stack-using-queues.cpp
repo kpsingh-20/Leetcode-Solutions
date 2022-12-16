7b6302a3e2b23809117f2a15ba67191690591750
kpsingh-20 <75653821+kpsingh-20@users.noreply.github.com>
2022-12-16 11:02:44 +0530

    Time: 4 ms (28.87%), Space: 7 MB (30.97%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -40,6 +40,8 @@ public:
 //     }
     
     
+////////////////////////////////////////////////////////////////////////////////////////////////////
+/*
     queue<int> q;
     MyStack() {
     }
@@ -68,6 +70,59 @@ public:
     void push(int x) {
         rev(q);
         func(q, x);
+    }
+    
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+    
+    int top() {
+        return q.front();
+    }
+    
+    bool empty() {
+        if(q.empty()) return 1;
+        return 0;
+    }
+*/
+    
+//////////////////////////////////////////////////////////////////////////////////////////
+    
+    queue<int> q;
+    MyStack() {
+    }
+    void func(queue<int> &q, int val){
+        if(q.empty()){
+            q.push(val);
+            return;
+        }
+        int x = q.front();
+        // cout << " " << x << endl;
+        q.pop();
+        func(q, val);
+        q.push(x);
+    }
+    
+    void rev(queue<int> &q){
+        if(q.empty()){
+            return;
+        }
+        int x = q.front();
+        q.pop();
+        rev(q);
+        q.push(x);
+    }
+    
+    void push(int x) {
+        q.push(x);
+        for(int i=0; i<q.size()- 1; i++){
+            q.push(q.front());
+            q.pop();
+        }
+    //     rev(q);
+    //     func(q, x);
     }
     
     int pop() {
