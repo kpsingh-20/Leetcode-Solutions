5ef27c6cd50b8b124b6f619634ab0e49bbbe1755
kpsingh-20 <75653821+kpsingh-20@users.noreply.github.com>
2023-01-08 15:11:35 +0530

    Time: 501 ms (72.86%), Space: 100.8 MB (27.86%) - LeetHub

--- 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -24,6 +24,7 @@ public:
 #define 	cinvvi(grid)			for(auto &x : grid){cinvec(x);}
 
 
+/*
 int minimumLines(vector<vector<int>>& a) 
 {
     if(a.size() == 1) return 0;
@@ -58,4 +59,55 @@ int minimumLines(vector<vector<int>>& a)
 return ans;
 }
 
+*/
+    
+    
+int minimumLines(vector<vector<int>>& a) 
+{
+	pii slope;
+
+	if(a.size() == 1) return 0;
+	if(a.size() == 2) return 1;
+
+	sort(all(a));
+	int n = a.size();
+	if(a[1][0] - a[0][0] == 0){
+		slope = {inf, inf};
+	}
+	else{
+		int dy = a[1][1] - a[0][1];
+		int dx = a[1][0] - a[0][0];
+		int g = __gcd(dx, dy);
+		slope = {dy/g, dx/g};
+		// slope = double(a[1][1] - a[0][1])/ double(a[1][0] - a[0][0]);
+	}
+
+
+
+	int ans = 1;
+	for(int i=2; i<n; i++){
+
+		pii curr;
+		if(a[i][0] - a[i-1][0] == 0){
+			curr = {inf, inf};
+		}
+		else{
+			int dy = a[i][1] - a[i-1][1];
+			int dx = a[i][0] - a[i-1][0];
+
+			int g = __gcd(dx, dy);
+			curr = {dy/g, dx/g};
+			// curr = double(a[i][1] - a[i-1][1])/ double(a[i][0] - a[i-1][0]);
+		}
+
+
+		// cout << slope << " " << curr;br;
+		if(curr != slope){
+			ans++;
+			slope = curr;
+		}
+	}
+
+return ans;
+}
 };
