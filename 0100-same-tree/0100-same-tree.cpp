75d223dd55b8d7f152daa4d3f379809c266aeda9
kpsingh-20 <75653821+kpsingh-20@users.noreply.github.com>
2023-01-10 17:06:05 +0530

    Time: 3 ms (65.62%), Space: 10.1 MB (8.85%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -11,6 +11,9 @@
  */
 class Solution {
 public:
+    
+    // Recursion
+/*
     bool isSameTree(TreeNode* p, TreeNode* q) {
         if(!p and !q) return 1;
         if(!p or !q) return 0;
@@ -19,5 +22,35 @@ public:
         
         return isSameTree(p->left, q->left) and isSameTree(p->right, q->right);
         
+    }
+*/
+    
+    #define Node TreeNode
+    bool isSameTree(Node* p, Node* q){
+        queue<Node*> pque, qque;
+        
+        pque.push(p);
+        qque.push(q);
+        
+        while(!pque.empty() and !qque.empty())
+        {
+            Node* f = pque.front(); pque.pop();
+            Node* s = qque.front(); qque.pop();
+            
+            if(!f and !s) continue;
+            if(!f or !s) return 0;
+            
+            if(f->val != s->val) return 0;
+            
+            pque.push(f->left);
+            qque.push(s->left);
+
+            pque.push(f->right);
+            qque.push(s->right);
+
+        }
+        
+        if(pque.size() or qque.size()) return 0;
+        return 1;
     }
 };
